363904bc1e881bfe35b7bc6474334c5a6d005f27
HermannChoi <hiyunseok347@gmail.com>
2024-10-08 12:58:20 +0900

    [docs]pwa 시도 6

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,7 @@
 import withPWA from "next-pwa";
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  // reactStrictMode: true,
-};
+const nextConfig = {};
 
 export default withPWA({
   ...nextConfig,
