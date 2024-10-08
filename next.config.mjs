eb2bcc5d0d0ec1edd0f2b3bf8387b99cb05f0cfb
HermannChoi <hiyunseok347@gmail.com>
2024-10-08 12:38:32 +0900

    [docs]pwa 시도 5

--- next.config.mjs
+++ next.config.mjs
@@ -1,20 +1,11 @@
-import withPWAInit from "@ducanh2912/next-pwa";
+import withPWA from "next-pwa";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // reactStrictMode: true,
 };
 
-const withPWA = withPWAInit({
-  dest: "public",
-  // disable: process.env.NODE_ENV === "development",
-  // register: true,
-  // scope: "/app",
-  // sw: "service-worker.js",
-  //...
-});
-
-// Your Next config is automatically typed!
 export default withPWA({
-  nextConfig,
+  ...nextConfig,
+  dest: "public",
 });
