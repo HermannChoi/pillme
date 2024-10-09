55ce734e3ed11d69cc3f9dde27670394a59f9e37
HermannChoi <hiyunseok347@gmail.com>
2024-10-10 03:31:52 +0900

    [feat]홈페이지 주소에서 네비게이터 홈 무조건 활성화되게 구현

--- app/components/homePage/Header.tsx
+++ app/components/homePage/Header.tsx
@@ -4,8 +4,16 @@
 import Image from "next/image";
 import pill from "@/app/assets/svg/pill.svg";
 import { outlineSt } from "@/app/style/homePage/outlineSt";
+import useNavigatorStore from "@/app/store/layout/useNavigatorStore";
+import { useEffect } from "react";
 
 const Header = () => {
+  const { setWhichPage } = useNavigatorStore();
+
+  useEffect(() => {
+    setWhichPage("Home");
+  }, [setWhichPage]);
+
   return (
     <header css={outlineSt.header}>
       <Image src={pill} alt="pill" priority={true} css={outlineSt.pillImg} />
